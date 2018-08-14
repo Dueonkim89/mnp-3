4b098b4cce4f90708cd4349e17ab66668a29fff0
Dueon Kim <dueonkim89@Yahoo.com>
2018-08-14 00:14:31 -0700

    set up server and post route handling for todo

--- server/server.js
+++ server/server.js
@@ -1,7 +1,35 @@
-var { mongoose } = require('./mongoDB/mongoose');
-var { Todo } = require('./model/todo');
-var { User } = require('./model/user');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const { mongoose } = require('./mongoDB/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+	//console.log(req.body);
+	
+	var todo = new Todo({
+		text: req.body.text
+	});
+	
+	todo.save().then( (todo) => {
+		res.send(todo);
+	}, (error) => {
+		res.status(404).send(error);
+	});
+	
+});
+
+
+
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
 
 
 
