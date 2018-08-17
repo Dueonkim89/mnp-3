184106439cc63e8ac5c1257d6a5103df1d7f1949
Dueon Kim <dueonkim89@Yahoo.com>
2018-08-16 18:29:00 -0700

    use heroku config variable to load db in prod mode

--- server/mongoDB/mongoose.js
+++ server/mongoDB/mongoose.js
@@ -4,7 +4,7 @@ const keys = require("./../../config/keys.js");
 mongoose.Promise = global.Promise;
 
 if (process.env.NODE_ENV === 'production') {
-	mongoose.connect(keys.productionMongoDB, { useNewUrlParser: true });
+	mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 } else {
 	mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 }
