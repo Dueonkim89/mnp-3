ac86fa1fb100591dbe14e23eb63485f2d643862c
Dueon Kim <dueonkim89@Yahoo.com>
2018-08-16 18:01:09 -0700

    different db based on production mode

--- server/mongoDB/mongoose.js
+++ server/mongoDB/mongoose.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const keys = require("../../config/keys.js");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'production') {
+	mongoose.connect(keys.productionMongoDB, { useNewUrlParser: true });
+} else {
+	mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+}
+
 
 module.exports = {
 	mongoose
