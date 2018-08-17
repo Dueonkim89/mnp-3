b2e6e1690d2f8a873fac0536be028f17a23e84f3
Dueon Kim <dueonkim89@Yahoo.com>
2018-08-16 18:07:45 -0700

    fix path routing for keys

--- server/mongoDB/mongoose.js
+++ server/mongoDB/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-const keys = require("../../config/keys.js");
+const keys = require("./../../config/keys.js");
 
 mongoose.Promise = global.Promise;
-
-console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === 'production') {
 	mongoose.connect(keys.productionMongoDB, { useNewUrlParser: true });
