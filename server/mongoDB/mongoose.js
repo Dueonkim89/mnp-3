a3a0ef28cfb822ddfeec1ca5ec8c450c31b7f949
Dueon Kim <dueonkim89@Yahoo.com>
2018-08-16 21:54:29 -0700

    fix deployment issue

--- server/mongoDB/mongoose.js
+++ server/mongoDB/mongoose.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
-const keys = require("./../../config/keys.js");
+const keys = require("../../config/keys.js");
 
 mongoose.Promise = global.Promise;
 
-if (process.env.NODE_ENV === 'production') {
-	mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
-} else {
-	mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
-}
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
 
 
 module.exports = {
